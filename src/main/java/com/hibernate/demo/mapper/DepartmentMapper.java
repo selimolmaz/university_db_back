8d58f8c3fbc546c5970d9d09cd11052297c1e5c6
selim-olmaz <selimolmaz3.1415@gmail.com>
2024-11-05 17:48:25 +0300

    mapper struct added

--- src/main/java/com/hibernate/demo/mapper/DepartmentMapper.java
+++ src/main/java/com/hibernate/demo/mapper/DepartmentMapper.java
@@ -2,24 +2,12 @@ package com.hibernate.demo.mapper;
 
 import com.hibernate.demo.dto.DepartmentDTO;
 import com.hibernate.demo.model.Department;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
 
-@Component
-public class DepartmentMapper {
+@Mapper(componentModel = "spring")
+public interface DepartmentMapper {
 
-    public DepartmentDTO toDTO(Department department) {
-        DepartmentDTO dto = new DepartmentDTO();
-        dto.setDeptName(department.getDeptName());
-        dto.setBuilding(department.getBuilding());
-        dto.setBudget(department.getBudget());
-        return dto;
-    }
+    DepartmentDTO toDTO(Department department);
 
-    public Department toEntity(DepartmentDTO dto) {
-        Department department = new Department();
-        department.setDeptName(dto.getDeptName());
-        department.setBuilding(dto.getBuilding());
-        department.setBudget(dto.getBudget());
-        return department;
-    }
+    Department toEntity(DepartmentDTO dto);
 }
