c64cda2ee07f901e72804c91ccd4dfcde2f95cf0
selim-olmaz <selimolmaz3.1415@gmail.com>
2024-11-07 02:02:24 +0300

    boşluklar ve kullanılmayan importlar temizlendi

--- src/main/java/com/hibernate/demo/repository/AdvisorRepository.java
+++ src/main/java/com/hibernate/demo/repository/AdvisorRepository.java
@@ -1,17 +1,13 @@
 package com.hibernate.demo.repository;
 
 import com.hibernate.demo.model.Advisor;
-import com.hibernate.demo.model.Instructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface AdvisorRepository extends JpaRepository<Advisor, String> {
-
-
     Advisor findBySid(String sId);
     List<Advisor> findByIid(String iId);
 }
