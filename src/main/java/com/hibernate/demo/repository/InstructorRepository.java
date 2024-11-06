9ba1b0666475de6034616574c9ee21ad1de47363
selim-olmaz <selimolmaz3.1415@gmail.com>
2024-11-06 22:08:47 +0300

    instructor yeniden yazılacak

--- src/main/java/com/hibernate/demo/repository/InstructorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hibernate.demo.repository;
-
-import com.hibernate.demo.model.Instructor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InstructorRepository extends JpaRepository<Instructor, String> {
-}
