3d4b8c9fbc32e6fc471846c04abb21a1a04b74e0
selim-olmaz <selimolmaz3.1415@gmail.com>
2024-11-07 02:02:40 +0300

    constructorler eklendi

--- src/main/java/com/hibernate/demo/dto/DepartmentDTO.java
+++ src/main/java/com/hibernate/demo/dto/DepartmentDTO.java
@@ -1,9 +1,14 @@
 package com.hibernate.demo.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class DepartmentDTO {
     private String deptName;
     private String building;
